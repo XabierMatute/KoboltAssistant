75d961cf2e20af3c2f2a4cebe8fe0f3214caf8a0
xmatute- <xmatute-@student.42urduliz.com>
2025-07-12 17:26:51 +0200

    pagina de nombres

--- caravana_enana/lib/screens/names.dart
+++ caravana_enana/lib/screens/names.dart
@@ -47,138 +47,168 @@ class _NamesScreenState extends State<NamesScreen> {
 
   void _sortNamesAlphabetically() {
     setState(() {
-      _isSortedAlphabetically = !_isSortedAlphabetically;
-      if (_isSortedAlphabetically) {
+      if (!_isSortedAlphabetically) {
         _names.sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
       } else {
         _refreshNames(); // Vuelve al orden original
       }
+      _isSortedAlphabetically = !_isSortedAlphabetically;
+
     });
   }
 
+  Future<void> _editName(BuildContext context, Name name) async {
+    print('Editing name : ${name.name}');
+    showDialog(
+      context: context,
+      builder: (context) => editNamePopUpBuilder(context, name),
+    );
+  }
+
+  Future<void> _addName(BuildContext context) async {
+    print('Adding a new name ');
+    showDialog(
+      context: context,
+      builder: (context) => addNamePopUpBuilder(context),
+    );
+
+  }
+
+    Widget addNamePopUpBuilder(context) {
+      final TextEditingController nameController = TextEditingController();
+      final TextEditingController descriptionController = TextEditingController();
+    
+      return AlertDialog(
+        title: const Text('Añadir Nombre'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Nombre'),
+            ),
+            TextField(
+              controller: descriptionController,
+              decoration: const InputDecoration(labelText: 'Descripción'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('Cancelar'),
+            // texto rojo
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red, // Color del texto
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                final newName = Name(
+                  id: DateTime.now().millisecondsSinceEpoch, // Genera un ID único
+                  name: nameController.text,
+                  description: descriptionController.text,
+                );
+                await NameTable.insertName(newName);
+                Navigator.of(context).pop();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Nombre añadido')),
+                );
+                _refreshNames(); // Refresca la lista después de añadir
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Error al añadir nombre: $e')),
+                );
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primary, // Color del botón
+              foregroundColor: Colors.white, // Color del texto
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // Bordes redondeados
+              ),
+            ),
+            child: const Text('Guardar'),
+          ),
+
+        ],
+      );
+    }
+
+  Widget editNamePopUpBuilder(BuildContext context, Name name) {
+    final TextEditingController nameController = TextEditingController(text: name.name);
+    final TextEditingController descriptionController = TextEditingController(text: name.description);
+
+    return AlertDialog(
+      title: const Text('Editar Nombre'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(labelText: 'Nombre'),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: const InputDecoration(labelText: 'Descripción'),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.red, // Color del texto
+          ),
+          child: const Text('Cancelar'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            try {
+              final updatedName = Name(
+                id: name.id,
+                name: nameController.text,
+                description: descriptionController.text,
+              );
+              await NameTable.updateName(updatedName.id, updatedName.name, updatedName.description);
+              Navigator.of(context).pop();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Nombre actualizado')),
+              );
+              _refreshNames(); // Refresca la lista después de editar
+            } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Error al actualizar el nombre: $e')),
+              );
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.primary, // Color del botón
+            foregroundColor: Colors.white, // Color del texto
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8), // Bordes redondeados
+            ),
+          ),
+          child: const Text('Guardar'),
+        ),
+      ],
+    );
+  }
+
   Future<void> _deleteName(BuildContext context, int id) async {
+    print('Deleting name with id: $id');
     try {
       await NameTable.deleteNameById(id);
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Nombre eliminado exitosamente')),
+        SnackBar(content: Text('Nombre eliminado')),
       );
-      _refreshNames(); // Refresca la lista después de eliminar
+      _refreshNames();
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error al eliminar el nombre: $e')),
       );
     }
-  }
-
-  Future<void> _editName(BuildContext context, Name name) async {
-    showDialog(
-      context: context,
-      builder: (context) {
-        final TextEditingController nameController =
-            TextEditingController(text: name.name);
-        final TextEditingController descriptionController =
-            TextEditingController(text: name.description);
-
-        return AlertDialog(
-          title: const Text('Editar Nombre'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: 'Nombre'),
-              ),
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(labelText: 'Descripción'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                try {
-                  await NameTable.updateName(
-                    name.id,
-                    nameController.text,
-                    descriptionController.text,
-                  );
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Nombre editado exitosamente')),
-                  );
-                  _refreshNames(); // Refresca la lista después de editar
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Error al editar el nombre: $e')),
-                  );
-                }
-              },
-              child: const Text('Guardar'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Cancelar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _addName(BuildContext context) async {
-    showDialog(
-      context: context,
-      builder: (context) {
-        final TextEditingController nameController = TextEditingController();
-        final TextEditingController descriptionController = TextEditingController();
-
-        return AlertDialog(
-          title: const Text('Añadir Nombre'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: 'Nombre'),
-              ),
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(labelText: 'Descripción'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                try {
-                  final newName = Name(
-                    id: DateTime.now().millisecondsSinceEpoch, // Genera un ID único
-                    name: nameController.text,
-                    description: descriptionController.text,
-                  );
-                  await NameTable.insertName(newName);
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Nombre añadido exitosamente')),
-                  );
-                  _refreshNames(); // Refresca la lista después de añadir
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Error al añadir el nombre: $e')),
-                  );
-                }
-              },
-              child: const Text('Guardar'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Cancelar'),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
@@ -198,39 +228,7 @@ class _NamesScreenState extends State<NamesScreen> {
       ),
       body: FutureBuilder<List<Name>>(
         future: _namesFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No hay nombres en la base de datos.'));
-          } else {
-            return ListView.builder(
-              itemCount: _names.length,
-              itemBuilder: (context, index) {
-                final name = _names[index];
-                return ListTile(
-                  title: Text(name.name),
-                  subtitle: Text(name.description),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () => _editName(context, name),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () => _deleteName(context, name.id),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          }
-        },
+        builder: nameListBuilder,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _addName(context),
@@ -238,4 +236,38 @@ class _NamesScreenState extends State<NamesScreen> {
       ),
     );
   }
+
+  Widget nameListBuilder(context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No hay nombres en la base de datos.'));
+        } else {
+          return ListView.builder(
+            itemCount: _names.length,
+            itemBuilder: (context, index) {
+              final name = _names[index];
+              return ListTile(
+                title: Text(name.getTitle()),
+                subtitle: Text(name.getSubtitle()),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () => _editName(context, name),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => _deleteName(context, name.id),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      }
 }
